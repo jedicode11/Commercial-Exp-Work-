f66244ed9eccc560fe98091d4dbe66ccab0e57f6
jedicode11 <92606781+jedicode11@users.noreply.github.com>
2024-12-17 13:01:29 +0200

    Delete CraftCMS Project/opt directory

--- CraftCMS Project/opt/hash-password.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Assuming Yii2's security component is autoloadable or you're doing this within a CraftCMS plugin/controller
-require 'vendor/autoload.php';
-
-$security = new \yii\base\Security();
-$hash = $security->generatePasswordHash($argv[1]);
-
-echo $hash."\n";
